cace1c49d86e3e072b7bfe39d1018d0d8dbd0783
Zane Thorn <zane.thorn@gmail.com>
2026-02-28 15:09:43 -0500

    code complete

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ import time
 import os
 import sys
 
-subreddit = 'dotnet'
-
 BASE_URL = 'https://www.reddit.com/'
 API_URL= 'https://oauth.reddit.com/'
-HEADERS = {'User-Agent': 'PyRedditTracker by Certain_Albatross'}
+HEADERS = {'User-Agent': 'RedditTracker by Certain_Albatross'}
 
 POSTS = {}
+
+AUTHOR_COUNT = os.getenv("TRACKER_AUTHOR_COUNT", 5)
+POST_COUNT = os.getenv("TRACKER_POST_COUNT", 5)
 
 # async I/O stuff
 post_lock = threading.Lock()
@@ -28,14 +29,14 @@ def update_post_statistics():
                 author_counts[author] = author_counts.get(author, 0) + post['ups']
         ordered_posts = sorted(author_counts.items(), key = lambda i: i[1], reverse=True)
         print("\nAuthor Statistics:")
-        for author, value in ordered_posts[:5]:
+        for author, value in ordered_posts[:AUTHOR_COUNT]:
             print(f"{author} - {value}")
 
         with post_lock:
             ordered_posts = sorted(POSTS.values(), key=operator.itemgetter('ups'), reverse=True)
 
         print("\nPost Statistics:")
-        for post in ordered_posts[:5]:
+        for post in ordered_posts[:POST_COUNT]:
             print(f"{post['title']} - {post['ups']} ({post['ups'] - post['downs']})")
 
 
@@ -56,8 +57,8 @@ def _make_throttled_request( url):
         return data
 
 def get_acess_token():
-    data = {'grant_type': 'password', 'username': 'Certain_Albatross', 'password': 'iby5SC%X^Azq73%b'}
-    auth = requests.auth.HTTPBasicAuth('7rYi6JTlp1Cno_3IwIPhGg', 'npQCEFf4xnHLxlZ2SfiXf6RsmaLlIA')
+    data = {'grant_type': 'password', 'username': os.getenv("TRACKER_USERNAME"), 'password': os.getenv("TRACKER_PASSWORD")}
+    auth = requests.auth.HTTPBasicAuth(os.getenv("TRACKER_APP_ID"), os.getenv("TRACKER_APP_SECRET"))
     response = requests.post(BASE_URL + 'api/v1/access_token', data=data, headers=HEADERS, auth=auth)
     response.raise_for_status()
     data = response.json()
@@ -79,7 +80,7 @@ async def main():
     access_token = get_acess_token()
     # Use the access token in the Authorization header
     HEADERS['Authorization'] = f'Bearer {access_token}'
-    t1 = threading.Thread(target=poll_for_new_posts, args=(subreddit,), daemon=True)
+    t1 = threading.Thread(target=poll_for_new_posts, args=(os.getenv("TRACKER_SUBREDDIT", "startrek"),), daemon=True)
     t2 = threading.Thread(target=update_post_statistics, daemon=True)
     t1.start()
     t2.start()
